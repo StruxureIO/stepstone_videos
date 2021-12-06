49a930bb2a2fe3ea23a7cef2be518cede7e98231
Alan Pasho <apasho@gmail.com>
2021-12-06 12:48:57 -0300

    updated favorites javascript code to add/remove checked class

--- views/videos/case-studies.php
+++ views/videos/case-studies.php
@@ -93,7 +93,7 @@ $this->registerJs("
   
   $(document).on('click', '.step-favorite', function (e) {
     e.stopImmediatePropagation();
-    var favorite_icon = $(this).find('span.fa.fa-star')
+    var favorite_icon = $(this).find('svg.svg-inline--fa.fa-star')
     var user_id = $(this).attr('data-user');
     var video_id  = $(this).attr('data-video');
     if( $(favorite_icon).hasClass('checked') ) {
--- views/videos/popular.php
+++ views/videos/popular.php
@@ -5,10 +5,14 @@ use yii\grid\GridView;
 use yii\widgets\Pjax;
 use humhub\modules\stepstone_videos\widgets\VideosMenu;
 use yii\db\Query;
+use humhub\modules\content\helpers\ContentContainerHelper;
 
 //include "protected/modules/stepstone_videos/widgets/VideosMenu.php";
 
 \humhub\modules\stepstone_videos\assets\Assets::register($this);
+
+$container = ContentContainerHelper::getCurrent();
+$container_guid = ($container) ? $container->guid : null;
 
 ?>
 
@@ -80,6 +84,7 @@ $this->registerJs("
       'url' : '$ajax_popular',
       'dataType' : 'json',
       'data' : {
+        'cguid' : '$container_guid',      
         '$csrf_param' : '$csrf_token',
         'page' : page  
       },
@@ -98,6 +103,7 @@ $this->registerJs("
       'url' : '$ajax_favorite',
       'dataType' : 'html',
       'data' : {
+        'cguid' : '$container_guid',      
         '$csrf_param' : '$csrf_token',
         'user_id' : user_id,
         'video_id' : video_id,
@@ -138,6 +144,7 @@ $this->registerJs("
       'url' : '$ajax_search_popular',
       'dataType' : 'json',
       'data' : {
+        'cguid' : '$container_guid',      
         '$csrf_param' : '$csrf_token',
         'tag' : 'all',
         'tag_name' : 'All Tags',
